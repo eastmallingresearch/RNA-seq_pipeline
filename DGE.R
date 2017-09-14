eba527fc85a0afe4292067564f5f990df43c9480
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-09-14 16:33:19 +0100

    Update DGE.R

--- DGE.R
+++ DGE.R
@@ -8,7 +8,7 @@ register(MulticoreParam(12))
 library(ggplot2)
 library(Biostrings)
 library(devtools)
-load_all("~/pipelines/metabarcoding/myfunctions") # this contains various R scripts for plotting graphs
+load_all("~/pipelines/metabarcoding/myfunctions")
 library(data.table)
 library(dplyr)
 library(naturalsort)
@@ -38,21 +38,15 @@ write.table(m[,1:6,with=F],"genes.txt",sep="\t",quote=F,row.names=F)
 #       Read pre-prepared colData,  countData and annotations
 ##=========================================================================================
 
+# Read sample metadata	    
 colData <- read.table("colData",header=T,sep="\t")
-# colData$condition <- rep(c("02780","02793","F55","10170","MWT","MOL","MKO","TJ"),3) # need to test this - will set columns to numbers 
 
-countData <- read.table("countData",sep="\t",header=T,row.names=1) # produced above, could just subset the data table countData <- m[,c(1,7:length(m),with=F]	
-countData <- countData[,colData$SampleID] # reorder countData columns to same order as colData rows
-
-# read annotaions from file 26/05/17    
-annotations <- fread("WT_annotation_ncbi.tsv")
-
-# remove .tx from annotation gene names	    
-annotations$query_id <- sub("\\.t.*","",annotations$query_id) 
-
-# remove duplicates (multiple exons with same annotation)	    
-annotations <- unique(annotations)
+# Get counts from m, or load 	    
+countData <- data.frame(m[,c(7:length(m)),with=F],row.names=m$Geneid) #or	    
+#countData <- fread("countData",sep="\t",header=T,row.names=1) 
 	    
+countData <- countData[,ccolData$SampleID] # reorder countData columns to same order as colData rows 
+
 #===============================================================================
 #       DESeq2 analysis
 #		Set alpha to the required significance level. This also effects how
@@ -64,12 +58,16 @@ annotations <- unique(annotations)
 # create DESeq object from count and column data
 dds <- 	DESeqDataSetFromMatrix(countData,colData,~1) 
 	    
+#### Technical replicates only ####
+	    
 # add grouping factor to identify technical replicates	    
 dds$groupby <- paste(dds$condition,dds$sample,sep="_")
 
 # sum replicates (must use same library or library size correction will go wonky)	    
 dds <- collapseReplicates(dds,groupby=dds$groupby)
-	    
+
+#### end technical replicates ####   
+	    	    
 # normalise counts for different library size (do after collapsing replicates)
 sizeFactors(dds) <- sizeFactors(estimateSizeFactors(dds)) 
 
@@ -77,7 +75,7 @@ sizeFactors(dds) <- sizeFactors(estimateSizeFactors(dds))
 design=~condition
 
 # add design to DESeq object	    
-design(dds) <- design # could just replace the ~1 in the first step with the design, if you really wanted to...
+design(dds) <- design
 
 # Run the DESeq statistical model	    
 dds <- DESeq(dds,parallel=T)
@@ -88,12 +86,8 @@ alpha <- 0.05
 # calculate the differences - uses the "levels" of the condition factor as the third term for the contrast
 # res is a list object containing the DESeq results objects for each contrast
 # contrast=c("condition","RH1","RH2") etc. (the below just runs through all of the different sample types (excluding RH1))
-res <- lapply(seq(2,8), function(i) results(dds,alpha=alpha,contrast=c("condition",levels(dds$condition)[i],"RH1")))
+res <- results(dds,alpha=alpha)
 
-# rename columns to nutrient type	      
-names(res) <- c("02793","F55","10170","MWT","MOL","MKO","TJ")
-		# RH2,   RH3,  RH4,    RH5,  RH6,  RH7,  RH8
-	
 # merge results with annotations
 res.merged <- lapply(res,function(x) left_join(rownames_to_column(as.data.frame(x)),annotations,by=c("rowname"="query_id")))	
 	
