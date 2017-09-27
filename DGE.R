5c982bbb5bd2c41c59b1c5a5541d191583be6021
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-09-27 11:57:37 +0100

    Update DGE.R

--- DGE.R
+++ DGE.R
@@ -15,10 +15,8 @@ library(naturalsort)
 library(tibble)
 
 #===============================================================================
-#       Load count data 
+#       Load data from fetureCounts
 #===============================================================================
-
-### featureCounts ###
 
 # load tables into a list of data tables - "." should point to counts directory, e.g. "counts/."
 # depending on how your count files are named and stored (in subdirectories or not) the .*txt$ regex and recursive flag may need editing
@@ -33,35 +31,56 @@ invisible(lapply(seq(1:length(qq)), function(i) colnames(qq[[i]])[7]<<-strsplit(
 m <- Reduce(function(...) merge(..., all = T,by=c("Geneid","Chr","Start","End","Strand","Length")), qq)
 
 # output "countData"
-write.table(m[,c(1,7:(ncol(m))),with=F],"countData",sep="\t",na="",quote=F,row.names=F) 
+# write.table(m[,c(1,7:(ncol(m))),with=F],"countData",sep="\t",na="",quote=F,row.names=F) 
 	    
 # output gene details
 write.table(m[,1:6,with=F],"genes.txt",sep="\t",quote=F,row.names=F) 
 
+# Get counts from m
+countData <- data.frame(m[,c(7:length(m)),with=F],row.names=m$Geneid)	    
 	    
-### SALMON ###
 
-# load tables into a list of data tables - "." should point to counts directory, e.g. "counts/."
-# for Salmon, all count files will have the same name (quant.sf) but be in seperate folder - so:	    
-qq <- lapply(list.files(".","quant.sf",full.names=T,recursive=T),function(x) fread(x)) 
-	     
-# unlike featureCounts the counts column header does contain information about the sample (named 	     
-names(qq) <- list.files(".","quant.sf",full.names=T,recursive=T)
+#===============================================================================
+#       Load data from SALMON quasi mapping
+#===============================================================================
+
+library(tximport)
+library(rjson)
+library(readr)
+
+# import transcript to gene mapping info
+tx2gene <- read.table("trans2gene.txt",header=T,sep="\t")
+
+# import quantification files
+# txi.reps <- tximport(list.files(".","quant.sf",full.names=T,recursive=T),type="salmon",tx2gene=tx2gene,txOut=T)
+# get the sample name form the file path - if not run from the "counts" folder the ...=2 may need to change
+# if you're following the pipeline exactly the sample name will be the second last file depth
+# e.g /home/bob/myproject/data/counts/MYSAMPLE/quan.   ... O.k. change of plan though the below is very cool so I've left it here    
+# mysamples <- rapply(strsplit(list.files(".","quant.sf",full.names=T,recursive=T),"\\/"), f=`[[`, ...=2, how="unlist")
+
+# import quantification files	    
+txi.reps <- tximport(paste(list.dirs(".",full.names=F,recursive=F),"/quant.sf",sep=""),type="salmon",tx2gene=tx2gene,txOut=T)	    
 	    
-	    
+# get the sample names from the folders	    
+mysamples <- list.dirs(".",full.names=F,recursive=F)
+
+# summarise to gene level (this can be done in the tximport step, but syntax is cleaner in two steps)
+txi.genes <- summarizeToGene(txi.reps,tx2gene)
+
+# set the sample names for txi.genes
+invisible(sapply(seq(1,3), function(i) colnames(txi.genes[[i]])<<-mysamples))
+
 #==========================================================================================
-#       Read pre-prepared colData,  countData and annotations
+#       Read pre-prepared sample metadata and annotations
 ##=========================================================================================
 
 # Read sample metadata	    
 colData <- read.table("colData",header=T,sep="\t")
+    
+# countData <- fread("countData",sep="\t",header=T,row.names=1) 
+countData <- countData[,colData$SampleID] # reorder countData columns to same order as colData rows 
 
-# Get counts from m, or load 	    
-countData <- data.frame(m[,c(7:length(m)),with=F],row.names=m$Geneid) #or	    
-#countData <- fread("countData",sep="\t",header=T,row.names=1) 
-	    
-countData <- countData[,ccolData$SampleID] # reorder countData columns to same order as colData rows 
-
+		 
 #===============================================================================
 #       DESeq2 analysis
 #		Set alpha to the required significance level. This also effects how
@@ -70,8 +89,11 @@ countData <- countData[,ccolData$SampleID] # reorder countData columns to same o
 #		alpha to 0.01
 #================================================================================
 
-# create DESeq object from count and column data
+# create DESeq object from featureCounts counts and sample metadata
 dds <- 	DESeqDataSetFromMatrix(countData,colData,~1) 
+
+# or create from Salmon counts and sample metadata		 
+dds <- DESeqDataSetFromTximport(txi.genes,colData,~1)	    
 	    
 #### Technical replicates only ####
 	    
